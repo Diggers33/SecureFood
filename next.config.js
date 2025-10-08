279d28b7395ea94fcf6885222a0fa3c9539348d3
Diggers33 <cdigby@iris-eng.com>
2025-10-08 17:51:55 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 // next.config.js
 const isProd = process.env.NODE_ENV === 'production';
-const repo = 'SecureFood'; // your repo name
+const repo = 'SecureFood'; // exact repo name and casing
 
 module.exports = {
   output: 'export',
   images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
-  trailingSlash: true, // <--- add this
+  trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : '',   // <—
+  },
 };
+
